b3d2e65e7867e83349483a2422f9fe6db4291257
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-24 01:10:41 +0800

    Use path as key for NavBarElement

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { childRoutes } from "./Routes";
 import NavBarElement from "../components/NavBarElement";
-import { v4 as uuid } from "uuid";
 import NavButtons from "../components/NavButtons";
 export default function Root() {
     return (
@@ -11,7 +10,7 @@ export default function Root() {
                     <ul>
                         {childRoutes.map((route, index) => (
                             <NavBarElement
-                                key={uuid()}
+                                key={route.path}
                                 index={index + 1} //switch from 0-based to 1-based indexing
                                 name={route.name}
                                 path={route.path}
