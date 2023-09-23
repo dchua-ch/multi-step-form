95ce2180f430978a7582706cb68c8266f83bdcae
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-23 21:12:41 +0800

    Implement markup for Info and Plan pages

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,7 +1,28 @@
 const Info = () => {
-  return (
-    <div>Info</div>
-  )
-}
+    return (
+        <>
+            <h1>Personal Info</h1>
+            <p>Please provide your name, email address, and phone number.</p>
 
-export default Info
+            <div>
+                <label htmlFor="name">Name</label>
+                <br />
+                <input name="name"></input>
+            </div>
+
+            <div>
+                <label htmlFor="email_address">Email Address</label>
+                <br />
+                <input name="email_address"></input>
+            </div>
+
+            <div>
+                <label htmlFor="phone_number">Phone Number</label>
+                <br />
+                <input name="phone_number"></input>
+            </div>
+        </>
+    );
+};
+
+export default Info;
--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,7 +1,47 @@
 const Plan = () => {
-  return (
-    <div>Plan</div>
-  )
-}
+    return (
+        <>
+            <h1>Select your plan</h1>
+            <p>You have the option of monthly or yearly billing</p>
 
-export default Plan
+            <div>
+                <input type="radio" name="plan" value="arcade" />
+                <label htmlFor="arcade"> Arcade, $9/month</label>
+            </div>
+
+            <div>
+                <input type="radio" name="plan" value="advanced" />
+                <label htmlFor="advanced"> Advanced, $12/month</label>
+            </div>
+
+            <div>
+                <input type="radio" name="plan" value="Pro" />
+                <label htmlFor="Pro"> Pro, $15/month</label>
+            </div>
+
+            <div>
+                <h2>Billing Frequency</h2>
+
+                <div>
+                    <input
+                        type="radio"
+                        name="billing_frequency"
+                        value="monthly"
+                    />
+                    <label htmlFor="monthlu"> Monthly</label>
+                </div>
+
+                <div>
+                    <input
+                        type="radio"
+                        name="billing_frequency"
+                        value="yearly"
+                    />
+                    <label htmlFor="yearly"> Yearly</label>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Plan;
