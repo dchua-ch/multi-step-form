11c6b3213eee09c51a95052bf79798125e692dc3
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-24 03:26:48 +0800

    Use typescript Partial type
    This is to allow payload action to contain a subset of the properties of FormState.

--- src/store/formSlice.ts
+++ src/store/formSlice.ts
@@ -1,15 +1,12 @@
 import { createSlice,PayloadAction } from "@reduxjs/toolkit";
 
 interface FormState {
-    // Each field is optional to make it easier to update form in our reducer
-    // i.e. to facilitate return {...state, ...action.payload}
-    // There is probably a better way to do this
-    name?: string;
-    emailAddress?: string;
-    phoneNumber?: string;
-    plan?: string;
-    billingFrequency?: string;
-    addOns?: string[];
+    name: string;
+    emailAddress: string;
+    phoneNumber: string;
+    plan: string;
+    billingFrequency: string;
+    addOns: string[];
 }
 const initialState: FormState = {
     name: "",
@@ -24,7 +21,7 @@ export const formSlice = createSlice({
     name: "form",
     initialState,
     reducers: {
-        updateForm: (state, action:PayloadAction<FormState>) => {
+        updateForm: (state, action:PayloadAction<Partial<FormState>>) => {
             return {
                 ...state,
                 ...action.payload,
