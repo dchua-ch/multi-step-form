b93634f8cf4882e368bdc3e59cdb4bece6397a4b
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-23 21:20:05 +0800

    Install uuid library and use it to generate unique key prop

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { childRoutes } from "./Routes";
 import NavBarElement from "../components/NavBarElement";
+import { v4 as uuid } from "uuid";
 export default function Root() {
     return (
         <>
@@ -9,6 +10,7 @@ export default function Root() {
                     <ul>
                         {childRoutes.map((route, index) => (
                             <NavBarElement
+                                key={uuid()}
                                 index={index + 1} //switch from 0-based to 1-based indexing
                                 name={route.name}
                                 path={route.path}
