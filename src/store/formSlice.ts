b7fe86f27bb3d2d7be0de4aa63b24b3f682bfcba
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-24 03:04:16 +0800

    Added types to redux store

--- src/store/formSlice.ts
+++ src/store/formSlice.ts
@@ -1,26 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+
+interface FormState {
+    // Each field is optional to make it easier to update form in our reducer
+    // i.e. to facilitate return {...state, ...action.payload}
+    // There is probably a better way to do this
+    name?: string;
+    emailAddress?: string;
+    phoneNumber?: string;
+    plan?: string;
+    billingFrequency?: string;
+    addOns?: string[];
+}
+const initialState: FormState = {
+    name: "",
+    emailAddress: "",
+    phoneNumber: "",
+    plan: "",
+    billingFrequency: "",
+    addOns: [""],
+};
 
 export const formSlice = createSlice({
     name: "form",
-    initialState: {
-        name: "",
-        emailAddress: "",
-        phoneNumber: "",
-        plan:"",
-        billingFrequency:"",
-        addOns:[""]
-    },
+    initialState,
     reducers: {
-        updateForm: (state, action) => {
+        updateForm: (state, action:PayloadAction<FormState>) => {
             return {
                 ...state,
-                ...action.payload
+                ...action.payload,
             };
         },
     },
 });
 
-
-export const {updateForm} = formSlice.actions;
+export const { updateForm } = formSlice.actions;
 
 export default formSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import formReducer from './formSlice'
-export default configureStore({
+const store =  configureStore({
     reducer:{
         form: formReducer
     },
 });
+export default store;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
