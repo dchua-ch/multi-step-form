48c508773c3c0f08492864a93f75ce64a05dd3b8
dchua-ch <82234823+dchua-ch@users.noreply.github.com>
2023-09-24 03:24:20 +0800

    Implement summary page by subscribing to redux store

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,8 +1,23 @@
+import { useAppSelector } from "../store/storeHooks";
+import {useNavigate} from "react-router-dom";
 const Summary = () => {
+    const form = useAppSelector((state) => state.form);
+    const navigate = useNavigate();
+    const handleSubmit = () => {
+      console.log(form);
+    }
     return (
         <>
             <h1>Finishing up</h1>
             <p>Double-check everything looks OK before confirming.</p>
+            <div>
+                {form.plan} ({form.billingFrequency})
+            </div>
+            {form.addOns?.map((addOn) => (
+                <div>{addOn}</div>
+            ))}
+            <button onClick={()=>navigate("/addons")}>Go Back</button>
+            <button onClick={handleSubmit}>Confirm</button>
         </>
     );
 };
